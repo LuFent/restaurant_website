a0c8951779b9faeb55669e1705d4a4eb88bbf26a
LuFent <vladmarchenkosteam@gmail.com>
2022-07-05 12:45:56 +0000

    deploy script update

--- deploy_star_burger.sh
+++ deploy_star_burger.sh
@@ -14,29 +14,16 @@ git pull
 echo -e "${BLUE}Collecting packages${NC}"
 pip3 install -r requirements.txt
 
-npm install
-
-
-echo -e "${BLUE}Building JS${NC}"
-./node_modules/.bin/parcel watch bundles-src/index.js --dist-dir bundles --public-url="./"
-
 echo -e "${BLUE}Collecting static${NC}"
-venv/bin/python3 manage.py collectstatic
-
-echo -e "${BLUE}Making migrations${NC}"
-python3 manage.py makemigrations
+venv/bin/python3 manage.py collectstatic --noinput
 
 python3 manage.py migrate
 
 
 echo -e "${BLUE}Restarting servers${NC}"
-systemctl stop nginx
-
 systemctl stop burgers
 
 systemctl daemon-reload
-
-systemctl start nginx
 
 systemctl start burgers
 
@@ -44,3 +31,8 @@ git_commit=$(git rev-parse HEAD)
 
 curl -H "X-Rollbar-Access-Token: d945324a58034cfa956594309698eac1" -H "Content-Type: application/json" -X POST 'https://api.rollbar.com/api/1/deploy' \
  -d '{"environment":"production", "revision": "$git_commit", "rollbar_username": "vladmarchenkosteam",  "status": "succeeded"}'
+
+
+npm install
+echo -e "${BLUE}Building JS${NC}"
+./node_modules/.bin/parcel watch bundles-src/index.js --dist-dir bundles --public-url="./"
