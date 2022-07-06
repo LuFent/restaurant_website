0521ea326e63243136348133aec26af6e9315ca9
LuFent <vladmarchenkosteam@gmail.com>
2022-07-06 16:11:19 +0000

    js fix

--- deploy_star_burger.sh
+++ deploy_star_burger.sh
@@ -17,14 +17,17 @@ pip3 install -r requirements.txt
 echo -e "${BLUE}Collecting static${NC}"
 venv/bin/python3 manage.py collectstatic --noinput
 
-python3 manage.py migrate
+python3 manage.py migrate --noinput
 
+./node_modules/.bin/parcel build bundles-src/index.js --dist-dir bundles --public-url="./"
+
+echo -e "${BLUE}Building JS${NC}"
 
 echo -e "${BLUE}Restarting servers${NC}"
 systemctl stop burgers
 
 systemctl reload nginx
-systemctl daemon-reload
+dsystemctl daemon-reload
 
 systemctl start burgers
 
@@ -32,8 +35,3 @@ git_commit=$(git rev-parse HEAD)
 
 curl -H "X-Rollbar-Access-Token: d945324a58034cfa956594309698eac1" -H "Content-Type: application/json" -X POST 'https://api.rollbar.com/api/1/deploy' \
  -d '{"environment":"production", "revision": "$git_commit", "rollbar_username": "vladmarchenkosteam",  "status": "succeeded"}'
-
-
-npm install
-echo -e "${BLUE}Building JS${NC}"
-./node_modules/.bin/parcel watch bundles-src/index.js --dist-dir bundles --public-url="./"
